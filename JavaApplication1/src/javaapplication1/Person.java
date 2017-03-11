82767d3e388113a9eccef7997596bd31391beca7
Hemonth.Mandava <Hemonth.Mandava@SSTECHLAPTOP365.sstech.internal>
2017-03-11 11:30:41 -0600

    Add new parameter salary in person object.

--- JavaApplication1/src/javaapplication1/Person.java
+++ JavaApplication1/src/javaapplication1/Person.java
@@ -13,6 +13,7 @@ public class Person {
     protected String name;
     protected String age;
     protected String dob;
+    protected long salary;
 
     public String getName() {
         return name;
@@ -37,6 +38,15 @@ public class Person {
     public void setDob(String dob) {
         this.dob = dob;
     }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+    
     
     
     
