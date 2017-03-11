00071de1c3e09183654b62a857cc2e5a1ee935b3
Hemonth.Mandava <Hemonth.Mandava@SSTECHLAPTOP365.sstech.internal>
2017-03-11 13:11:13 -0600

    Create Vendor class

---
+++ JavaApplication1/src/javaapplication1/Vendor.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Hemonth.Mandava
+ */
+public class Vendor {
+    protected String name;
+    protected Date startdate;
+    protected Date enddate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(Date startdate) {
+        this.startdate = startdate;
+    }
+
+    public Date getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(Date enddate) {
+        this.enddate = enddate;
+    }
+    
+}
