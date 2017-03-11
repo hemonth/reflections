3a6e362e9f085a8ac56e844ca51bdfca4560505f
Hemonth.Mandava <Hemonth.Mandava@SSTECHLAPTOP365.sstech.internal>
2017-03-11 11:07:17 -0600

    New Person entity class created.

---
+++ JavaApplication1/src/javaapplication1/Person.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Hemonth.Mandava
+ */
+public class Person {
+    protected String name;
+    protected String age;
+    protected String dob;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+    
+    
+    
+}
