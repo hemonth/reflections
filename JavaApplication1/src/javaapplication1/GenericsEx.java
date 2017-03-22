59ac3a223cad5e333f479b917065e148b7ed05c5
Hemonth Mandava <Hemonth.Mandava@sstech.internal>
2017-03-22 15:09:32 -0500

    add changes made.

---
+++ JavaApplication1/src/javaapplication1/GenericsEx.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Hemonth.Mandava
+ */
+public class GenericsEx {
+    
+    public static void main(String [] args){
+        
+        System.out.println("hello hemonth");
+        
+        Integer i = 1000;
+        
+        int count = i.byteValue();
+        System.out.println("count:"+count);
+    }
+}
